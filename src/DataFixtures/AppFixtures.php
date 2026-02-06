2f840279d7cebb8cf3d433e47d5da2e5bf04d2e4
Rija Lalaina <rijalaraz@gmail.com>
2026-02-06 03:06:05 +0300

    Book fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Book;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Création d'une vingtaine de livres ayant pour titre
+        for ($i=0; $i < 20; $i++) { 
+            $book = new Book();
+            $book->setTitle("Livre " . $i);
+            $book->setCoverText("Quatrième de couverture numéro : ". $i);
+            $manager->persist($book);
+        }
 
         $manager->flush();
     }
