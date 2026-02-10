e375ca1a28eca31395a10aa045a56df5a77d981c
Rija Lalaina <rijalaraz@gmail.com>
2026-02-10 04:15:33 +0300

    Downgrade Symfony

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,34 @@ namespace App\DataFixtures;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
+        // Création d'un utilisateur normal
+        $user = new User();
+        $user->setEmail('user@bookapi.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
+        // Création d'un utilisateur admin
+        $admin = new User();
+        $admin->setEmail('admin@bookapi.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'password'));
+        $manager->persist($admin);
+
         // Création des auteurs.
         $listAuthor = [];
         for ($i=0; $i < 10; $i++) { 
