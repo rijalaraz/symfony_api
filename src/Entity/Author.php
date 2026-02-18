7e1fcc9f3fb7adcb307e35f4882a27a7502257b5
Rija Lalaina <rijalaraz@gmail.com>
2026-02-18 21:27:52 +0300

    API Platform

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -2,27 +2,38 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AuthorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\Groups as SerializerGroups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['author:view']],
+    denormalizationContext: ['groups' => ['author:create', 'author:update']]
+)]
 class Author
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     #[Groups(["book:view","book:create"])]
+    #[SerializerGroups(["author:view"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
     #[Groups(["book:view"])]
+    #[SerializerGroups(["author:view","author:create","author:update"])]
+    #[Assert\NotBlank(message: "Last name is required.")]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     #[Groups(["book:view"])]
+    #[SerializerGroups(["author:view","author:create","author:update"])]
     private ?string $firstName = null;
 
     /**
